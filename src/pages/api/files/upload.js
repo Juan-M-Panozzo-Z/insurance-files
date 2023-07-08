c50029dd4e02c978836e524c1230b7b442171a9d
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-07-07 23:16:55 -0300

    modified:   .env modified:   src/pages/api/files/upload.js

--- src/pages/api/files/upload.js
+++ src/pages/api/files/upload.js
@@ -22,6 +22,7 @@ export default async function handle(req, res) {
             });
             const client = new S3Client({
                 region: process.env.AWS_REGION,
+                endpoint: process.env.AWS_ENDPOINT,
                 credentials: {
                     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
                     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
