c13749b326880e4a68e42db43789e35378bab4ae
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-07-17 07:55:31 -0300

    modified:   src/pages/components/CardFiles.js

--- src/pages/components/CardFiles.js
+++ src/pages/components/CardFiles.js
@@ -2,10 +2,9 @@ import Link from "next/link";
 
 export default function CardFiles({ files }) {
     return (
-        <div className="grid md:grid-cols-4 gap-4 p-4 items-start">
+        <div className="grid md:grid-cols-4 gap-4 p-4  justify-items-center">
             {files?.map((file) => (
-                <div className="p-4" key={file.id}>
-                    <div className="card md:w-96 bg-neutral-600 text-neutral-content ">
+                    <div key={file.id} className="card w-full bg-neutral-600 text-neutral-content ">
                         <div className="card-body  text-center">
                         <h3 className="card-title pb-1 mb-1 border-b-2">{file.type}</h3>
                         
@@ -21,7 +20,6 @@ export default function CardFiles({ files }) {
                         ))}
                         </div>
                     </div>
-                </div>
             ))}
         </div>
     );
