907d3acca262519e2786e38d2e9617390be31ee8
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-07-19 12:11:12 -0300

    modificados:     src/pages/components/Drawer.jsx

--- src/pages/components/Drawer.jsx
+++ src/pages/components/Drawer.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
-import { useSession, signOut } from "next-auth/react";
+import {signOut } from "next-auth/react";
 
 const links = [
     {
@@ -14,8 +14,6 @@ const links = [
     },
 ];
 function Drawer({ children }) {
-    const { data: session, status } = useSession({ required: true });
-    console.log(session);
     const router = useRouter();
 
     const isLoginPage = router.pathname === "/login";
